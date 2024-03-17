e484ea0939a8dc887dc5be36ddc0ca7dbcdbe099
arthurlfig <arthurklfig@gmail.com>
2024-03-17 20:21:33 -0300

    sim

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,3 +1,4 @@
+#calculadora
 A = float(input("digite o primeiro numero: "))
 B = float(input("digite o segundo numero: "))
 operação = input("digite o tipo de operação sendo 1- soma 2-subtração 3-multiplicação 4-divisão 5-sair : ")
@@ -10,4 +11,3 @@ elif operação == "3":
      print(A * B)
 elif operação == "4":
     print(A / B)
-    
