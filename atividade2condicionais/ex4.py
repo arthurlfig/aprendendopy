d5debd63c7647fd877015b5f4ff5d0b52217d51d
arthurlfig <arthurklfig@gmail.com>
2024-03-19 20:18:04 -0300

    sim

--- atividade2condicionais/ex4.py
+++ atividade2condicionais/ex4.py
@@ -1,11 +1,11 @@
-n1 = float(int(" digite sua primeira nota"))
-n2 = float(int(" digite sua segunda nota"))
-n3 = float(int(" digite sua  terceira nota 1"))
-n4 = float(int(" digite sua quarta nota "))
+n1 = float(input(" digite sua primeira nota"))
+n2 = float(input(" digite sua segunda nota"))
+n3 = float(input(" digite sua  terceira nota 1"))
+n4 = float(input(" digite sua quarta nota "))
 media = (n1 + n2 + n3 + n4 ) / 4 
 if media >= 7:
     print(f"vc foi aprovado(a)")
 elif media >= 4 and media <7:
     print(" vai de recuperação")
 else:
-    print("Reprovado!!")
+    print("Reprovado!!!")
